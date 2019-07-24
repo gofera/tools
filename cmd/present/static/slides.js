84286803896161abf50341640e87e3d955c2f49f
dxu <dean.xu@asml.com>
2019-07-24 10:51:05 +0800

    add font.css instead query from google

--- cmd/present/static/slides.js
+++ cmd/present/static/slides.js
@@ -500,8 +500,7 @@ function addFontStyle() {
   var el = document.createElement('link');
   el.rel = 'stylesheet';
   el.type = 'text/css';
-  el.href = '//fonts.googleapis.com/css?family=' +
-            'Open+Sans:regular,semibold,italic,italicsemibold|Droid+Sans+Mono';
+  el.href = urlPrefix + PERMANENT_URL_PREFIX + 'font.css';
 
   document.body.appendChild(el);
 };
