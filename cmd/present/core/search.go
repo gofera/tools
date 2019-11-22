0525c7cef1fd2a31652c24f166b73581da3ebf1e
dxu <dean.xu@asml.com>
2019-11-22 15:18:22 +0800

    search add section number, add api doc

--- cmd/present/core/search.go
+++ cmd/present/core/search.go
@@ -2,7 +2,7 @@ package present_core
 
 import (
 	"encoding/json"
-	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"os/exec"
@@ -21,7 +21,7 @@ func searchHandler(writer http.ResponseWriter, request *http.Request) {
 		http.Error(writer, "keyword must provide", http.StatusBadRequest)
 		return
 	}
-	cmd := exec.Command("grep", "-rine", keyword, *contentPath, "--exclude-dir=.git", "--include=*.slide")
+	cmd := exec.Command("grep", "-rine", keyword, filepath.Join(*contentPath, "users"), "--exclude-dir=.git", "--include=*.slide")
 	cmd.Stderr = os.Stderr
 	output, err := cmd.Output()
 	if err != nil {
@@ -37,7 +37,6 @@ func searchHandler(writer http.ResponseWriter, request *http.Request) {
 	resultMap := make(map[string]*SearchRecord)
 	result := make([]*SearchRecord, 0)
 	for _, line := range lines {
-		fmt.Println(line)
 		s1 := strings.SplitN(line, ".slide:", 2)
 		if len(s1) != 2 {
 			continue
@@ -49,27 +48,26 @@ func searchHandler(writer http.ResponseWriter, request *http.Request) {
 			http.Error(writer, "Fail to extract search result line: "+err.Error(), http.StatusInternalServerError)
 			return
 		}
-		text := s2[1]
+		line -- // grep line start from 1
 		if record, ok := resultMap[path]; ok {
-			record.Lines = append(record.Lines, SearchLine{
-				Line: line,
-				Text: text,
-			})
+			record.addLine(line)
 		} else {
 			relative, err := filepath.Rel(*contentPath, path)
 			if err != nil {
 				http.Error(writer, "Fail to calc url: "+err.Error(), http.StatusInternalServerError)
 				return
 			}
-			record := SearchRecord{
-				Path: filepath.ToSlash(relative),
-				Lines: []SearchLine{
-					{
-						Line: line,
-						Text: text,
-					},
-				},
+			bs, err := ioutil.ReadFile(path)
+			if err != nil {
+				http.Error(writer, "Fail to read file: "+err.Error(), http.StatusInternalServerError)
+				return
 			}
+			content := string(bs)
+			record := SearchRecord{
+				Path:    filepath.ToSlash(relative),
+				content: strings.Split(content, "\n"),
+			}
+			record.addLine(line)
 			result = append(result, &record)
 			resultMap[path] = &record
 		}
@@ -85,11 +83,28 @@ func searchHandler(writer http.ResponseWriter, request *http.Request) {
 }
 
 type SearchRecord struct {
-	Path  string
-	Lines []SearchLine
+	Path    string
+	Lines   []SearchLine
+	content []string
 }
 
 type SearchLine struct {
-	Line int
-	Text string
+	Line    int
+	Section int
+	Text    string
+}
+
+func (s *SearchRecord) addLine(line int) {
+	text := s.content[line]
+	section := 0
+	for i := 0; i < line; i++ {
+		if strings.HasPrefix(s.content[i], "* ") {
+			section++
+		}
+	}
+	s.Lines = append(s.Lines, SearchLine{
+		Line:    line,
+		Text:    text,
+		Section: section,
+	})
 }
